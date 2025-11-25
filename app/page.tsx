7429d91f6d302b43906829b9b54ac2fd7c122d6a
brenohs <breno.hs@outlook.com>
2025-11-25 10:47:12 -0300

    feat: implementa pagina home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
+
 const Page = () => {
   return (
     <section>
@@ -7,6 +11,24 @@ const Page = () => {
       <p className="text-center mt-5">
         Hackatons, Meetups, and Conferences, All in One Place
       </p>
+      <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {events.map(({ slug, image, title, location, date, time }) => (
+            <li key={slug}>
+              <EventCard
+                slug={slug}
+                title={title}
+                image={image}
+                location={location}
+                date={date}
+                time={time}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
