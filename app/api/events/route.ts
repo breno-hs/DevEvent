894caf852d09646c8ed3d6c6809c68f021a4325d
Breno Henrique de Souza <breno.souza@v2solucoes.com.br>
2025-11-26 11:38:54 -0300

    feat: inicia implementacao das rotas de api

---
+++ app/api/events/route.ts
@@ -0,0 +1,41 @@
+import Event from "@/database/event.model";
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const formData = await req.formData();
+
+    let event;
+
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json(
+        {
+          message: "Invalid JSON data format",
+        },
+        { status: 400 }
+      );
+    }
+
+    const createdEvent = await Event.create(event);
+
+    return NextResponse.json(
+      { message: "Event created successfully", event: createdEvent },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: "Event Creation Failed",
+        error: error instanceof Error ? error.message : "Unknown",
+      },
+      { status: 500 }
+    );
+  }
+}
