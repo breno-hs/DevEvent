8ca365da88b6f7591a46de8f5c0c6047c9eaf783
brenohs <breno.hs@outlook.com>
2025-11-27 15:26:04 -0300

    fix: fixing for deploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
+import { events } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL!;
 
 const Home = async () => {
   "use cache";
   cacheLife("hours");
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await response.json();
 
   return (
     <section>
@@ -25,7 +26,7 @@ const Home = async () => {
         <ul className="events">
           {events &&
             events.length > 0 &&
-            events.map((event: IEvent) => (
+            events.map((event) => (
               <li key={event.title}>
                 <EventCard {...event} />
               </li>
